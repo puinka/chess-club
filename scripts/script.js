883d8ccf0164c5297c48f709e6ff670ec64afcd2
Valeria <vozzheva@mail.ru>
2022-06-09 18:18:43 +0100

    Adds function for selecting an indicator

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const galleries = document.querySelectorAll(".gallery");
+
+const gallery = galleries[1];
+const paginationIndicators = gallery.querySelectorAll(
+  ".gallery__page-indicator"
+);
+
+const sliderWrap = gallery.querySelector(".gallery__slides-wrap");
+
+function setActiveIndicator(indicators, number) {
+  indicators.forEach((item) => {
+    item.classList.remove("gallery__page-indicator_active");
+  });
+  indicators[number].classList.add("gallery__page-indicator_active");
+}
