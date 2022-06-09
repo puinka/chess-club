872f108dd56ca7faa16d1c9c61ea53bb0f1ae35e
Valeria <vozzheva@mail.ru>
2022-06-09 19:12:17 +0100

    Adds switch mobile slides on pagination click

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
 const galleries = document.querySelectorAll(".gallery");
 
 const gallery = galleries[1];
+
 const paginationIndicators = gallery.querySelectorAll(
   ".gallery__page-indicator"
 );
-
 const sliderWrap = gallery.querySelector(".gallery__slides-wrap");
+const slides = gallery.querySelectorAll(".gallery__slide");
+const slideWidth = slides[1].getBoundingClientRect().width;
+
+function getPosition(number) {
+  return (slideWidth + 20) * number;
+}
 
 function setActiveIndicator(indicators, number) {
   indicators.forEach((item) => {
@@ -13,3 +19,13 @@ function setActiveIndicator(indicators, number) {
   });
   indicators[number].classList.add("gallery__page-indicator_active");
 }
+
+function slideToPosition(indicators, number) {
+  setActiveIndicator(indicators, number);
+  const position = getPosition(number);
+  sliderWrap.scrollLeft = position;
+}
+
+paginationIndicators.forEach((item, i, arr) => {
+  item.addEventListener("click", () => slideToPosition(arr, i));
+});
